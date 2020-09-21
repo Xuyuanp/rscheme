243c3deaf75b83e78dfeab12f3b1a080ea66f60b
Xuyuan Pang <xuyuanp@gmail.com>
2020-09-21 20:22:09 +0800

    Added eval funcs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
-use std::fmt;
 use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Debug)]
+enum EvalError {
+    NotImplemented,
+}
+
+impl fmt::Display for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            EvalError::NotImplemented => write!(f, "{}", "NotImplemented"),
+        }
+    }
+}
+
+type EvalResult = Result<LispVal, EvalError>;
 
 type ValCtx = HashMap<&'static str, LispVal>;
-type FnCtx = HashMap<&'static str, LispVal>;
+type FnCtx = HashMap<&'static str, IFunc>;
+
+trait ApplyTrait {
+    fn apply(&mut self, name: &'static str, args: &Vec<LispVal>) -> EvalResult;
+}
+
+impl ApplyTrait for EnvCtx {
+    fn apply(&mut self, name: &'static str, args: &Vec<LispVal>) -> EvalResult {
+        match self.fenv.get(name) {
+            Some(ifunc) => ifunc(self, args),
+            None => Err(EvalError::NotImplemented),
+        }
+    }
+}
 
 #[allow(dead_code)]
-struct EnvCtx{
+#[derive(Clone)]
+struct EnvCtx {
     env: ValCtx,
     fenv: FnCtx,
 }
 
-type Eval<T> = fn(EnvCtx) -> T;
+type IFunc = fn(&mut EnvCtx, &Vec<LispVal>) -> EvalResult;
 
-type IFunc = fn(Vec<LispVal>) -> Eval<LispVal>;
+fn add(_ctx: &mut EnvCtx, args: &Vec<LispVal>) -> EvalResult {
+    match args.as_slice() {
+        [] => Ok(LispVal::Number(0)),
+        [n @ LispVal::Number(_)] => Ok(n.clone()),
+        [s @ LispVal::String(_)] => Ok(s.clone()),
+        [_v, _rest @ ..] => Err(EvalError::NotImplemented),
+    }
+}
 
 #[allow(dead_code)]
+#[derive(Clone)]
 enum LispVal {
     Atom(&'static str),
     List(Vec<LispVal>),
@@ -38,7 +75,10 @@ impl fmt::Display for LispVal {
             },
             LispVal::Nil => write!(f, "'()"),
             LispVal::List(vals) => write!(f, "[{}]", {
-                vals.iter().map(|v| v.to_string()).collect::<Vec<String>>().join(",")
+                vals.iter()
+                    .map(ToString::to_string)
+                    .collect::<Vec<String>>()
+                    .join(",")
             }),
             LispVal::Func(_) => write!(f, "(internal function)"),
             LispVal::Lambda(_, _) => write!(f, "(lambda function)"),
@@ -46,18 +86,50 @@ impl fmt::Display for LispVal {
     }
 }
 
+fn eval(ctx: &mut EnvCtx, val: &LispVal) -> EvalResult {
+    match val {
+        LispVal::List(lst) => match lst.as_slice() {
+            [LispVal::Atom("quota"), val] => Ok(val.clone()),
+            [LispVal::Atom(fn_name), args @ ..] => ctx.apply(fn_name, &args.to_vec()),
+            _ => Err(EvalError::NotImplemented),
+        },
+        LispVal::Atom(_) => Err(EvalError::NotImplemented),
+        LispVal::Func(_) => Err(EvalError::NotImplemented),
+        LispVal::Lambda(_, _) => Err(EvalError::NotImplemented),
+        _ => Ok(val.clone()),
+    }
+}
+
 fn main() {
     use LispVal as lv;
-    let vals = lv::List(vec![
-        lv::Atom("fuck"),
-        lv::Number(10),
-        lv::Bool(true),
-        lv::Bool(false),
-        lv::String("foo"),
-        lv::Nil,
-        lv::List(vec![
-            lv::Atom("bar"),
-        ]),
-    ]);
-    println!("{}", vals)
+    // let vals = lv::List(vec![
+    //     lv::Atom("fuck"),
+    //     lv::Number(10),
+    //     lv::Bool(true),
+    //     lv::Bool(false),
+    //     lv::String("foo"),
+    //     lv::Nil,
+    //     lv::List(vec![lv::Atom("bar")]),
+    // ]);
+    // println!("{}", vals);
+
+    let mut ctx = EnvCtx {
+        env: ValCtx::new(),
+        fenv: FnCtx::new(),
+    };
+    ctx.fenv.insert("+", add);
+    println!("{}", eval(&mut ctx, &lv::String("fuck")).unwrap());
+    println!("{}", eval(&mut ctx, &lv::Number(2333)).unwrap());
+    println!(
+        "{}",
+        eval(
+            &mut ctx,
+            &lv::List(vec![lv::Atom("quota"), lv::Number(42),])
+        )
+        .unwrap()
+    );
+    println!(
+        "{}",
+        eval(&mut ctx, &lv::List(vec![lv::Atom("+"), lv::Number(10)])).unwrap()
+    );
 }
